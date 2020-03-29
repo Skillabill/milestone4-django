325ae6da3b7c91635272f3c3ebc261e0b0ed8e58
Mark Kirwan <47473819+Skillabill@users.noreply.github.com>
2020-03-29 15:27:52 +0100

    add pillow

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ.setdefault('STRIPE_PUBLISHABLE', "pk_test_fzv9wlq4ZeiIe5s1wni44Wuf00Hj93HS5p")
+os.environ.setdefault('STRIPE_SECRET', "sk_test_VLbVsFxhMbS4kzucsdae66sO00XMw99gue")
--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from .views import all_products
 
-urlpatterns= [
+urlpatterns = [
     url(r'^$', all_products, name='products')
 ]
